e9011cb2b3a222b4faf823b8fc4cd1f6516fd0b5
Furkan TÜRKER <furkanturkertr@gmail.com>
2024-10-01 22:36:28 +0300

    İlk commit

---
+++ HesapMakinasi/HesapMakinasi/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        MaskedTextBox1 = New MaskedTextBox()
+        MaskedTextBox2 = New MaskedTextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(154, 82)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(34, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Sayi1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(154, 129)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(34, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Sayi2"
+        ' 
+        ' MaskedTextBox1
+        ' 
+        MaskedTextBox1.Location = New Point(248, 79)
+        MaskedTextBox1.Name = "MaskedTextBox1"
+        MaskedTextBox1.Size = New Size(100, 23)
+        MaskedTextBox1.TabIndex = 2
+        ' 
+        ' MaskedTextBox2
+        ' 
+        MaskedTextBox2.Location = New Point(248, 129)
+        MaskedTextBox2.Name = "MaskedTextBox2"
+        MaskedTextBox2.Size = New Size(100, 23)
+        MaskedTextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(113, 187)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "TOPLA"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(260, 247)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "Label3"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(210, 187)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "CIKAR"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(306, 187)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 7
+        Button3.Text = "CARP"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(399, 187)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 8
+        Button4.Text = "BOL"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(MaskedTextBox2)
+        Controls.Add(MaskedTextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+
+End Class
---
+++ HesapMakinasi/HesapMakinasi/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Dim sayi1, sayi2 As Integer
+    Dim sonuc As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sayi1 = MaskedTextBox1.Text
+        sayi2 = MaskedTextBox2.Text
+        sonuc = sayi1 + sayi2
+        Label3.Text = "Sonuç: " & sonuc
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sayi1 = MaskedTextBox1.Text
+        sayi2 = MaskedTextBox2.Text
+        sonuc = sayi1 - sayi2
+        Label3.Text = "Sonuç: " & sonuc
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        sayi1 = MaskedTextBox1.Text
+        sayi2 = MaskedTextBox2.Text
+        sonuc = sayi1 * sayi2
+        Label3.Text = "Sonuç: " & sonuc
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        sayi1 = MaskedTextBox1.Text
+        sayi2 = MaskedTextBox2.Text
+        sonuc = sayi1 / sayi2
+        Label3.Text = "Sonuç: " & sonuc
+    End Sub
+
+End Class
