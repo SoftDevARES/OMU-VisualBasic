a2020aead9ed3564f29d184ca7a80d431d53dd33
Furkan TÜRKER <furkanturkertr@gmail.com>
2024-10-01 23:01:50 +0300

    Yeni dosya eklendi

---
+++ SatisFiyatiHesaplama/SatisFiyatiHesaplama/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        MaskedTextBox1 = New MaskedTextBox()
+        MaskedTextBox2 = New MaskedTextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(135, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(57, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Alış Fiyatı"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(135, 141)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Kar Oranı"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(233, 258)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Label3"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(177, 194)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(184, 27)
+        Button1.TabIndex = 3
+        Button1.Text = "Satış Fiyatını Hesapla"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' MaskedTextBox1
+        ' 
+        MaskedTextBox1.Location = New Point(222, 75)
+        MaskedTextBox1.Name = "MaskedTextBox1"
+        MaskedTextBox1.Size = New Size(100, 23)
+        MaskedTextBox1.TabIndex = 4
+        ' 
+        ' MaskedTextBox2
+        ' 
+        MaskedTextBox2.Location = New Point(222, 133)
+        MaskedTextBox2.Name = "MaskedTextBox2"
+        MaskedTextBox2.Size = New Size(100, 23)
+        MaskedTextBox2.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1034, 538)
+        Controls.Add(MaskedTextBox2)
+        Controls.Add(MaskedTextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+
+End Class
---
+++ SatisFiyatiHesaplama/SatisFiyatiHesaplama/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Dim alisfiyati, satisfiyati, karorani As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        alisfiyati = MaskedTextBox1.Text
+        karorani = MaskedTextBox2.Text
+        satisfiyati = ((alisfiyati * karorani) / 100) + alisfiyati
+        Label3.Text = "Satış Fiyatı: " & satisfiyati
+    End Sub
+End Class
